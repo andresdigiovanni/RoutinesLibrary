9380325a33569dce7402fc6c802d16b057f1faf8
Andrés Di Giovanni <agiovanni@jbctools.com>
2018-03-22 15:03:08 +0100

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutinesLibrary
+{
+    public class Class1
+    {
+    }
+}
