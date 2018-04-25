fb070233fe61d1b3685a144330538510dc6b65be
Andrés Di Giovanni <agiovanni@jbctools.com>
2018-04-25 09:21:46 +0200

    Corrected AesHelper error

--- src/Security/AesHelper.cs
+++ src/Security/AesHelper.cs
@@ -6,7 +6,7 @@ namespace RoutinesLibrary.Security
 {
     public class AesHelper
     {
-        public static byte[] EncryptStringToBytes_AES(string plainText, byte[] Key, byte[] IV)
+        public static byte[] Encrypt(byte[] plainText, byte[] Key, byte[] IV)
         {
             // Check arguments
             if (ReferenceEquals(plainText, null) || plainText.Length <= 0)
@@ -27,10 +27,11 @@ namespace RoutinesLibrary.Security
             byte[] encrypted = null;
 
             // Create an Aes object with the specified key and IV
-            using (System.Security.Cryptography.Aes aesAlg = System.Security.Cryptography.Aes.Create())
+            using (Aes aesAlg = AesManaged.Create())
             {
                 aesAlg.Key = Key;
                 aesAlg.IV = IV;
+                aesAlg.Padding = PaddingMode.Zeros;
 
                 // Create a decrytor to perform the stream transform.
                 ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
@@ -40,24 +41,20 @@ namespace RoutinesLibrary.Security
                 {
                     using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
                     {
-                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                        {
-                            // Write all data to the stream.
-                            swEncrypt.Write(plainText);
-                        }
-
-
-                        encrypted = msEncrypt.ToArray();
+                        csEncrypt.Write(plainText, 0, plainText.Length);
+                        csEncrypt.Flush();
+                        csEncrypt.Close();
                     }
+
+                    encrypted = msEncrypt.ToArray();
                 }
             }
-
 
             // Return the encrypted bytes from the memory stream
             return encrypted;
         }
 
-        public static string DecryptStringFromBytes_AES(byte[] cipherText, byte[] Key, byte[] IV)
+        public static byte[] Decrypt(byte[] cipherText, byte[] Key, byte[] IV)
         {
             // Check arguments
             if (ReferenceEquals(cipherText, null) || cipherText.Length <= 0)
@@ -76,13 +73,14 @@ namespace RoutinesLibrary.Security
             }
 
             // Declare the string used to hold the decrypted text
-            string plaintext = null;
+            byte[] clearBytes = null;
 
             // Create an Aes object with the specified key and IV
-            using (System.Security.Cryptography.Aes aesAlg = System.Security.Cryptography.Aes.Create())
+            using (Aes aesAlg = AesManaged.Create())
             {
                 aesAlg.Key = Key;
                 aesAlg.IV = IV;
+                aesAlg.Padding = PaddingMode.Zeros;
 
                 // Create a decrytor to perform the stream transform.
                 ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
@@ -90,18 +88,18 @@ namespace RoutinesLibrary.Security
                 // Create the streams used for decryption.
                 using (MemoryStream msDecrypt = new MemoryStream(cipherText))
                 {
-                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write))
                     {
-                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
-                        {
-                            // Read the decrypted bytes from the decrypting stream and place them in a string
-                            plaintext = srDecrypt.ReadToEnd();
-                        }
+                        csDecrypt.Write(cipherText, 0, cipherText.Length);
+                        csDecrypt.Flush();
+                        csDecrypt.Close();
                     }
+
+                    clearBytes = msDecrypt.ToArray();
                 }
             }
 
-            return plaintext;
+            return clearBytes;
         }
     }
 }
