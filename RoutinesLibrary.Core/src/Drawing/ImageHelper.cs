f79201e05d1ef8e531197c9662bb696999576634
Andrés Di Giovanni <agiovanni@jbctools.com>
2018-05-23 09:18:20 +0200

    Added ImageHelper

---
+++ RoutinesLibrary.Core/src/Drawing/ImageHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace RoutinesLibrary.Core.Drawing
+{
+    public class ImageHelper
+    {
+        public static Bitmap SetBackgroundColor(Bitmap bitmap, Color color)
+        {
+            Bitmap temp = new Bitmap(bitmap.Width, bitmap.Height, PixelFormat.Format32bppRgb);
+            temp.SetResolution(bitmap.HorizontalResolution, bitmap.VerticalResolution);
+            Graphics g = Graphics.FromImage(temp);
+            g.Clear(color);
+            g.DrawImage(bitmap, Point.Empty);
+
+            return temp;
+        }
+
+        public static Bitmap Resize(Bitmap bitmap, int width, int height)
+        {
+            Bitmap temp = new Bitmap(width, height, PixelFormat.Format32bppRgb);
+            temp.SetResolution(bitmap.HorizontalResolution, bitmap.VerticalResolution);
+            Graphics g = Graphics.FromImage(temp);
+            g.DrawImage(bitmap, 0, 0, width, height);
+
+            return temp;
+        }
+    }
+}
