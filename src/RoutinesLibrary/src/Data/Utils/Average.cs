d2033a3d6ee2acb228b721afc71318ffefc67a43
Andrés Di Giovanni <agiovanni@jbctools.com>
2018-07-13 12:53:30 +0200

    Added AverageTest

--- src/RoutinesLibrary/src/Data/Utils/Average.cs
+++ src/RoutinesLibrary/src/Data/Utils/Average.cs
@@ -8,8 +8,20 @@ namespace RoutinesLibrary.Data
 {
     public class Average
     {
+        /// <summary>
+        /// Calculate average
+        /// </summary>
+        /// <param name="previousAvg">Previous average</param>
+        /// <param name="count">Number of elements with the new value</param>
+        /// <param name="currentValue">New value</param>
+        /// <returns>Average</returns>
         public static double RunningAverage(double previousAvg, int count, double currentValue)
         {
+            if (count == 0)
+            {
+                throw (new DivideByZeroException("count"));
+            }
+
             return (previousAvg * (count - 1) + currentValue) / count;
         }
     }
