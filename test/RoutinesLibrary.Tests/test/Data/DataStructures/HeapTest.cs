84f0eaa10d951b1f3ebbdbed3df178ce66a29c84
Andrés Di Giovanni <agiovanni@jbctools.com>
2018-08-10 15:21:54 +0200

    Added HeapTest

---
+++ test/RoutinesLibrary.Tests/test/Data/DataStructures/HeapTest.cs
@@ -0,0 +1,57 @@
+﻿using RoutinesLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RoutinesLibrary.Tests.Data
+{
+    public class HeapTest
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10000)]
+        public void Insert(int values)
+        {
+            Heap<int> heap = new Heap<int>(1);
+            for (int i = 0; i < values; i++)
+            {
+                heap.Insert(i);
+            }
+            var result = heap.Count;
+
+            Assert.Equal(values, result);
+        }
+
+        [Fact]
+        public void IsEmpty()
+        {
+            Heap<int> heap = new Heap<int>(0);
+
+            Assert.True(heap.IsEmpty);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10000)]
+        public void Remove(int values)
+        {
+            Heap<int> heap = new Heap<int>(1);
+            for (int i = 0; i < values; i++)
+            {
+                heap.Insert(i);
+            }
+            for (int i = 0; i < values; i++)
+            {
+                heap.Remove();
+            }
+            var result = heap.Count;
+
+            Assert.True(heap.IsEmpty);
+        }
+    }
+}
