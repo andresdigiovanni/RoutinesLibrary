7eaf4ce4c2d45b02a1f754e101c96068747539fa
Andrés Di Giovanni <agiovanni@jbctools.com>
2018-11-29 09:56:51 +0100

    Added Lock class

---
+++ src/RoutinesLibrary/src/Lock/Lock.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RoutinesLibrary.Data
+{
+    public static class Lock
+    {
+        public static void LockTimeout(object oLock, int timeout, Action action)
+        {
+            Exception exception = null;
+            bool lockWasTaken = false;
+            try
+            {
+                Monitor.TryEnter(oLock, timeout, ref lockWasTaken);
+                if (lockWasTaken)
+                {
+                    action();
+                }
+                else
+                {
+                    throw new Exception("Timeout exceeded, unable to lock.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // conserver l'exception
+                exception = ex;
+            }
+            finally
+            {
+                // release le lock
+                if (lockWasTaken)
+                {
+                    Monitor.Exit(oLock);
+                }
+
+                // relancer l'exception
+                if (exception != null)
+                {
+                    throw new Exception("An exception occured during the lock proces.", exception);
+                }
+            }
+        }
+    }
+}
