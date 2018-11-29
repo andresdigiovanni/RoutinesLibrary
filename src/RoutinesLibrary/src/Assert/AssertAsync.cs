28e93fd0dc8976a98089712ed334eb2005ae05f8
Andrés Di Giovanni <agiovanni@jbctools.com>
2018-11-29 10:00:23 +0100

    Added AssertAsync

---
+++ src/RoutinesLibrary/src/Assert/AssertAsync.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutinesLibrary.Data
+{
+    public static class AssertAsync
+    {
+        public static void CompletesIn(int timeout, Action action)
+        {
+            var task = Task.Run(action);
+            var completedInTime = Task.WaitAll(new[] { task }, TimeSpan.FromSeconds(timeout));
+
+            if (task.Exception != null)
+            {
+                if (task.Exception.InnerExceptions.Count == 1)
+                {
+                    throw task.Exception.InnerExceptions[0];
+                }
+
+                throw task.Exception;
+            }
+
+            if (!completedInTime)
+            {
+                throw new TimeoutException($"Task did not complete in {timeout} seconds.");
+            }
+        }
+    }
+}
