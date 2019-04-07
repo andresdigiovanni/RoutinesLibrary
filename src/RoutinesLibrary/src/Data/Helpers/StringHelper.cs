8fb8ce5aefd332edf7df4e7b4e035335de953d59
Andrés Di Giovanni <andresdigiovanni@gmail.com>
2019-04-07 19:16:36 +0200

    Removed methods from StringHelper

--- src/RoutinesLibrary/src/Data/Helpers/StringHelper.cs
+++ src/RoutinesLibrary/src/Data/Helpers/StringHelper.cs
@@ -5,27 +5,6 @@ namespace RoutinesLibrary.Data
 {
     public class StringHelper
     {
-        public static string ToHex(string asciiString)
-        {
-            string hex = "";
-            foreach (char eachChar in asciiString)
-            {
-                hex += string.Format("{0:x2}", Convert.ToInt32(eachChar));
-            }
-            return hex;
-        }
-
-        public static string HexToString(string HexValue)
-        {
-            string StrValue = "";
-            while (HexValue.Length > 0)
-            {
-                StrValue += Convert.ToChar(Convert.ToUInt32(HexValue.Substring(0, 2), 16)).ToString();
-                HexValue = HexValue.Substring(2, HexValue.Length - 2);
-            }
-            return StrValue;
-        }
-
         public static string RemoveSpecialCharacters(string str)
         {
             StringBuilder sb = new StringBuilder();
